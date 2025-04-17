724b2abf547d3b72cda537c8d43b3ba4429e72b5
kaoru <kametyaso@gmail.com>
2025-04-17 12:41:23 +0900

    画像と文字の微修正

--- public_html/assets/js/index.js
+++ public_html/assets/js/index.js
@@ -25,3 +25,44 @@ jQuery(function () {
     return false;
   });
 });
+
+/** バニラJS
+ * // ハンバーガーボタンとドロワー
+document.getElementById("js-button-drawer").addEventListener("click", function () {
+  this.classList.toggle("is-checked");
+  const drawer = document.getElementById("js-drawer");
+  if (drawer.style.display === "none" || drawer.style.display === "") {
+    drawer.style.display = "block";
+  } else {
+    drawer.style.display = "none";
+  }
+  document.body.classList.toggle("is-fixed");
+});
+
+// 上部に戻るボタンの設置
+document.addEventListener("DOMContentLoaded", function () {
+  // コマンドボタンを隠す
+  const topBtn = document.getElementById("js-return-top");
+  topBtn.style.display = "none";
+
+  // スクロールが規定値に到達でボタン表示
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 80) {
+      topBtn.style.display = "block";
+    } else {
+      topBtn.style.display = "none";
+    }
+  });
+
+  // ボタン押すとトップに戻る
+  topBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+});
+ *
+ *
+ * **/
